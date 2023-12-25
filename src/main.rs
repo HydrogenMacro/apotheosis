1ccde3fa817a54e93bc10b07b283f679189393f6
HydrogenMacro <hydrogenmacro@gmail.com>
2023-12-24 22:04:31 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("hello world");
+    //let b = Board::from_raw([BoardSquare::WPawn; 64], White, [[true, true], [true, true]], 0, None);
+    let bb = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    println!("{:?}", bb);
+    //bb.print();
 }
