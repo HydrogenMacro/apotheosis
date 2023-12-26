9ba39714d0679d43acade4cf99d834d166328abb
HydrogenMacro <hydrogenmacro@gmail.com>
2023-12-26 17:09:57 -0500

    Fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 pub mod board;
-use crate::Board;
+use crate::board::Board;
 fn main() {
     let bb = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     // println!("{:?}", bb);
