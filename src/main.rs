639d84673b52b776b3be8abf3d037a317d5f78ab
HydrogenMacro <hydrogenmacro@gmail.com>
2023-12-26 16:49:35 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+pub mod board;
+use crate::Board;
 fn main() {
-    //let b = Board::from_raw([BoardSquare::WPawn; 64], White, [[true, true], [true, true]], 0, None);
     let bb = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    println!("{:?}", bb);
-    //bb.print();
+    // println!("{:?}", bb);
+    bb.print();
 }
