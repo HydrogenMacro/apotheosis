bec2735077d6b41c75672090ed47f5da5eaad14b
HydrogenMacro <hydrogenmacro@gmail.com>
2023-12-28 16:29:39 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,6 @@ fn main() {
     let bb = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     // println!("{:?}", bb);
     bb.print();
+    let vm = bb.get_valid_moves();
+    println!("{:?}", vm);
 }
