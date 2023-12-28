b9a8df31985099990944304072674f3095c007ce
HydrogenMacro <hydrogenmacro@gmail.com>
2023-12-27 21:39:23 -0500

    Update bootseq.sh

--- bootseq.sh
+++ bootseq.sh
@@ -1,6 +1,8 @@
-apk add cargo 
-git clone https://github.com/hydrogenmacro/apotheosis
-clear 
-printf "\n\n \x1b[2;37;41m ----- RUNNING APOTHEOSIS ----- \n\n"
-cd apotheosis 
-cargo run ; cd .. ; rm -r apotheosis
+apk add cargo;
+git clone https://github.com/hydrogenmacro/apotheosis;
+clear;
+printf "\n\n \x1b[2;37;41m ----- RUNNING APOTHEOSIS ----- \n\n";
+cd apotheosis;
+cargo run;
+cd ..;
+rm -r apotheosis
