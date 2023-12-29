462ed8130ed4c08dd0663083fefe2260b0b65b74
HydrogenMacro <hydrogenmacro@gmail.com>
2023-12-29 00:16:45 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-pub mod board;
-use crate::board::Board;
+use apotheosis::Board;
 fn main() {
     let bb = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     // println!("{:?}", bb);
