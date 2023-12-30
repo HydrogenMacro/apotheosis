72e431cff0e8136d6bed8921f4d4209554745e33
HydrogenMacro <hydrogenmacro@gmail.com>
2023-12-30 17:58:58 -0500

    Update board_tests.rs

--- tests/board_tests.rs
+++ tests/board_tests.rs
@@ -131,7 +131,7 @@ fn sq(board_square: &str) -> i8 {
     }
     panic!("board_square should only have 2 characters");
 }
-fn assert_vecs_are_permutations<T: Eq + Hash + Clone>(vec1: Vec<T>, vec2: Vec<T>) {
+fn assert_vecs_are_permutations<T: Eq + Hash + Clone + Debug>(vec1: Vec<T>, vec2: Vec<T>) {
     let set1: HashSet<T> = HashSet::from_iter(vec1.iter().cloned());
     let vecs_are_permutations = vec1.len() == vec2.len() && vec2.iter().all(|e| set1.contains(e));
     assert!(vecs_are_permutations, "{:?}\n is not a permutation of \n{:?}", vec1, vec2);
