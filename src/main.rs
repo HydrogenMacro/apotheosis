32dada15e22e0ef4520a895053915db7068489c4
HydrogenMacro <hydrogenmacro@gmail.com>
2024-01-05 19:27:07 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use apotheosis::board::Board;
 fn main() {
     let bb = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     // println!("{:?}", bb);
-    bb.print();
-    let vm = bb.get_valid_moves();
-    println!("{:?} with {} moves", vm, vm.len());
+    //let vm = bb.get_valid_moves();
+    //println!("{:?} with {} moves", vm, vm.len());
 }
