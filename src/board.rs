5e62997bb8d167401e4f58a10d267a85fa4e95e8
HydrogenMacro <hydrogenmacro@gmail.com>
2024-01-10 21:20:14 -0500

    Update board.rs

--- src/board.rs
+++ src/board.rs
@@ -104,7 +104,7 @@ impl BoardSquare {
             let square_in_dir = (self.pos() as i8 + ((dir.dx() * square_num as i8) + (dir.dy() * square_num as i8 * 8))) as u8;
             squares_in_direction.push(BoardSquare(square_in_dir));
         }
-        println!("from: {:?}, squares in direction: {}, squares: {:?}", self, amount_of_squares, squares_in_direction);
+        println!("from: {:?}, in dir: {:?}, squares in direction: {}, squares: {:?}", self, dir, amount_of_squares, squares_in_direction);
         return squares_in_direction;
     }
 }
