4687f2ad0e28822cecfd3678b795339978016c72
HydrogenMacro <hydrogenmacro@gmail.com>
2024-01-11 22:11:34 -0500

    fix parenteses

--- tests/board_tests.rs
+++ tests/board_tests.rs
@@ -14,5 +14,5 @@ use std::{
 fn pawn_movement_test() {
     let b = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     let moves = b.get_valid_moves();
-    println!("{:?}", moves.into_iter().for_each(|&m| println!("{:b}", m.0));
+    println!("{:?}", moves.into_iter().for_each(|&m| println!("{:b}", m.0)));
 }
