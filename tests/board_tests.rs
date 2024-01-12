6e87db89842fcde58e8c79881d6fef3deae0533d
HydrogenMacro <hydrogenmacro@gmail.com>
2024-01-12 08:16:40 -0500

    Update board_tests.rs

--- tests/board_tests.rs
+++ tests/board_tests.rs
@@ -13,6 +13,7 @@ use std::{
 #[test]
 fn pawn_movement_test() {
     let b = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    println!("{}", b);
     let moves = b.get_valid_moves();
     moves
         .into_iter()
